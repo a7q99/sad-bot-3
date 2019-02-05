b25c8e9bcd39e98418b5cd749d7f57efefca3ebe
a7q99 <46059490+a7q99@users.noreply.github.com>
2019-02-05 21:59:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(`سيبقى حذائي وكلام الناس أخوه للأبد')
+    client.user.setGame(`نتجاهل ليبقى الود بيننا ')
 
 });
 client.login(process.env.BOT_TOKEN);
